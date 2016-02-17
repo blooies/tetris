6b04cad50df6c1c48a0f981bfa2e42e675dc0b69
blooies <joy28@msn.com>
2016-02-17 12:45:27 -0500

    build grid with cells

---
+++ tetris.js
@@ -0,0 +1,72 @@
+// 1: grid and random generated pieces that move down w. preview of next piece
+// 2: can rotate pieces and move them in different directions - cant move them when hitting grid border or on top of another piece
+// 3: when row has been filled
+// 4: points
+
+//Grid, Cell, Piece, 
+// Cell knows what piece is on it
+// Piece knows what cells it is occupying
+// Grid holds cells
+
+// 0123456789
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+// 11
+// 12
+// 13
+// 14
+// 15
+// 16
+// 17
+// 18
+// 19
+
+Config = {
+    size: {
+        width: 10, //10 cells across
+        height: 20 //20 cells down
+    },
+    cellDefaultColor: '#fff'
+}
+
+var Grid = function() {
+    this.width = Config.size.width;
+    this.height = Config.size.height;
+    this.cells = [];
+    this.buildCells();
+}
+
+Grid.prototype.buildGrid = function() {
+}
+
+Grid.prototype.buildCells = function() {
+    for (var i=0; i<this.height; i++) {
+        var y = i;
+        for (var j=0; j<this.width; j++) {
+            var x = j;
+            var cell = new Cell(x, y);
+            this.cells.push(cell);
+        }
+    }
+}
+
+var Cell = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.color = Config.cellDefaultColor;
+    this.el = null;
+    this.buildHtml();
+}
+
+Cell.prototype.buildHtml = function() {
+    var el = document.createElement("div");
+    el.setAttribute("class", "cell");
+}
